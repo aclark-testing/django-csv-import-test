e84d394f80882ec886f5ac13e6ef6f648ff99624
Alex Clark <aclark@aclark.net>
2014-11-21 18:03:24 -0500

    Update(s)

--- django_csv_import/django_csv_import/settings.py
+++ django_csv_import/django_csv_import/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_csv_import.django_csv_import',
 )
 
 MIDDLEWARE_CLASSES = (
