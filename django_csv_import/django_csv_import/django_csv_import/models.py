3cf8470304eda7b9a24f246e1da114620cb6ae69
Alex Clark <aclark@aclark.net>
2014-11-21 18:31:44 -0500

    Update(s)

--- django_csv_import/django_csv_import/django_csv_import/models.py
+++ django_csv_import/django_csv_import/django_csv_import/models.py
@@ -7,14 +7,20 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    def __unicode__(self):
+        return ' '.join([self.first_name, self.last_name])
 
 class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     instrument = models.CharField(max_length=100)
+    def __unicode__(self):
+        return ' '.join([self.first_name, self.last_name])
 
 class Album(models.Model):
     artist = models.ForeignKey(Musician)
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     num_stars = models.IntegerField()
+    def __unicode__(self):
+        return self.name
