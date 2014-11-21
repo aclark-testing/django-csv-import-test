175a9dd2f178ea94530bc3c63da7f8eb7f910be5
Alex Clark <aclark@aclark.net>
2014-11-21 18:09:54 -0500

    Add file(s)

--- django_csv_import/django_csv_import/django_csv_import/admin.py
+++ django_csv_import/django_csv_import/django_csv_import/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from django_csv_test.django_csv_test.models import Album
-from django_csv_test.django_csv_test.models import Musician
-from django_csv_test.django_csv_test.models import Person
+from django_csv_import.django_csv_import.models import Album
+from django_csv_import.django_csv_import.models import Musician
+from django_csv_import.django_csv_import.models import Person
 
 class AlbumAdmin(admin.ModelAdmin):
     pass
